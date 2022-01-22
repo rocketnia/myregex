a0e79a12bb2b3ebf62518b4faa312e17e8988dab
Rocketnia <rokitna@hotmail.com>
2022-01-22 12:36:27 -0800

    Turn the MyRegexTest demo into a proper JUnit Jupiter unit test.

--- README.md
+++ README.md
@@ -8,12 +8,10 @@ Traditional regex systems require every lookbehind expression to have a maximum 
 
 Traditional regex systems have a backreference operator, to assert that a certain matched substring appears again at another location in the overall match. The mechanism I implemented here is somewhat more expressive: The parser combinators can collect multiple substring matches under a single name, and another combinator can take a look at this set and force a backtrack if not all the substrings are identical.
 
-A rudimentary unit test is provided in `com.rocketnia.hacks.myregex.MyRegexTest`. You can run it if you have a compiled version of the lib/src/main/java/ directory in your Java classpath.
-
 
 ## Installation and use
 
-To run Jisp, first install a distribution of the JDK. We've used Eclipse Temurin 17.0.1+12 from Adoptium, but Jisp should work even on much older JVM versions. (At the time of development in 2008, I was developing in Java 6 on the Sun Microsystems JDK.)
+To build MyRegex, first install a distribution of the JDK. We've used Eclipse Temurin 17.0.1+12 from Adoptium, but MyRegex should work even on much older JVM versions. (At the time of development in 2008, I was developing in Java 6 on the Sun Microsystems JDK.)
 
 Then, run a build with the Gradle wrapper.
 
@@ -21,10 +19,4 @@ Then, run a build with the Gradle wrapper.
 ./gradlew build
 ```
 
-This builds a JAR file in lib/build/lisbs/lib.jar.
-
-You can also run unit tests like so:
-
-```
-./gradlew run
-```
+This runs unit tests and creates a JAR file in lib/build/lisbs/lib.jar.
--- lib/src/test/java/com/rocketnia/hacks/myregex/MyRegexTest.java
+++ lib/src/test/java/com/rocketnia/hacks/myregex/MyRegexTest.java
@@ -1,11 +1,19 @@
 package com.rocketnia.hacks.myregex;
 
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
 import com.rocketnia.hacks.myregex.regex.*;
 import com.rocketnia.util.Integrity;
 
 public class MyRegexTest
 {
-    public static void main( String[] args )
+    @Test
+    public void backreferencesWork()
     {
 
         // These are the values being tested.
@@ -39,21 +47,40 @@ public class MyRegexTest
                 new RegexMacroAssertSingleValue( "surroundingPhrase" )
             );
 
-        System.out.println( "begin matches" );
-
+        List< String > reports = new ArrayList< String >();
         for ( RegexMatch match: TEST_REGEX.matches( TEST_SUBJECT, TEST_POSITION, new RegexContext() ) )
         {
-            System.out.println( "match: " + match.getPosition() + "-" + match.getEndPosition() + " [" + match.getContents() + "]" );
+            reports.add( "match: " + match.getPosition() + "-" + match.getEndPosition() + " [" + match.getContents() + "]" );
             for ( RegexMatch submatch: match.getContext().getMacroMatches( "surroundingPhrase" ) )
             {
-                System.out.println( "submatch: " + submatch.getPosition() + "-" + submatch.getEndPosition() + " [" + submatch.getContents() + "]" );
+                reports.add( "submatch: " + submatch.getPosition() + "-" + submatch.getEndPosition() + " [" + submatch.getContents() + "]" );
             }
         }
 
+        Assertions.assertEquals(
+            Arrays.< String >asList(
+                "match: 5-13 [ or not ]",
+                "submatch: 0-5 [to be]",
+                "submatch: 13-18 [to be]",
+                "match: 5-14 [ or not t]",
+                "submatch: 1-5 [o be]",
+                "submatch: 14-18 [o be]",
+                "match: 5-15 [ or not to]",
+                "submatch: 2-5 [ be]",
+                "submatch: 15-18 [ be]",
+                "match: 5-16 [ or not to ]",
+                "submatch: 3-5 [be]",
+                "submatch: 16-18 [be]",
+                "match: 5-17 [ or not to b]",
+                "submatch: 4-5 [e]",
+                "submatch: 17-18 [e]"
+            ),
+            reports
+        );
         System.out.println( "end matches" );
     }
 
-    public static Regex compile( String regexString )
+    private static Regex compile( String regexString )
     {
         Integrity.assertNotNull( regexString );
 
@@ -68,7 +95,7 @@ public class MyRegexTest
         }
     }
 
-    public static Regex compileWithSlashes( String regexString )
+    private static Regex compileWithSlashes( String regexString )
     {
         Integrity.assertNotNull( regexString );
 
